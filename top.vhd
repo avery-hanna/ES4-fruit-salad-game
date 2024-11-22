03dedab20a20316ba83e7b4888e93b1231e48619
avery-hanna <136497654+avery-hanna@users.noreply.github.com>
2024-11-23 00:26:13 +0100

    Update top.vhd
    Basic state logic

--- top.vhd
+++ top.vhd
@@ -8,16 +8,47 @@ entity top is
     );
 
   architecture toparch of top is
-    signal statevect: std_logic_vector(1 downto 0);
+  type STATE is (START, PLAY, DROP, OVER);
+  signal curr_state : STATE := START;
+  signal center : 
+  begin
 
 
     component predropcontroller is (
       state: in std_logic_vector(1 downto 0);
-      NES_controller: in std_logic_vector(7 downto 0)
+      NES_controller: in std_logic_vector(6 downto 0)
       );
 
     instance: predropcontroller
-      port map predropcontroller(NESinput=> NES_controller, statevect => state);
-    
+    port map predropcontroller(NES_controller, center, new_center);
+
+
+    -- State machine logic
+    process(clk) begin
+      if rising_edge(clk) then
+        begin case curr_state
+          when START =>
+              -- on any input switch to PLAY
+              if NES_controller /= 8b"0" then
+                  curr_state <= PLAY;
+              end if;
+          when PLAY =>
+              center <= new_center;
+              if NES_controller == "00000100" then -- clicked to drop
+                  curr_state <= DROP;
+          when DROP =>
+              
+          when OVER =>
+              time.wait(3);
+              curr_state <= START;
+          when others =>
+              curr_state <= START;
+              
+
+        end case;
+
+      end if;
+
+    end process;
 
   end;
