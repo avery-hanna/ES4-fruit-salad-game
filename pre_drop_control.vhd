833888d28f879ccaa2af0f1723c858069473cb89
eapanner <141077581+eapanner@users.noreply.github.com>
2024-11-22 18:26:32 -0500

    Update pre_drop_control.vhd

--- pre_drop_control.vhd
+++ pre_drop_control.vhd
@@ -4,8 +4,9 @@ use IEEE.std_logic_1164.all;
 
 entity pre_drop_control is 
   port(
-        state: in std_logic_vector(1 downto 0);
+        curr_center: in std_logic_vector(6 downto 0);
         NES_controller: in std_logic_vector (7 downto 0);
+        new_center: out std_logic_vector(7 downto 0)
     );
 
 
@@ -19,10 +20,13 @@ signal curr_center : unsigned(3 downto 0);
     -- read curr_center
     -- TODO don't update if at extreme
     if NES_controller == "00000010": -- left
-        curr_center -= 2;
+        if curr_center <= "0000010" then
+          curr_center <= curr_center;
+        else curr_center <= curr_center - 2;
   
     elsif NES_controller == "00000001": -- right
-        curr_center += 2;
+        if curr_
+        curr_center = curr_center + 2;
   
     -- else don't update
   
