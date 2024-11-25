4c625ebc574a3b23fa638d68d2461f642679d9f6
Sarah H. Grand <sgrand01@vm-hw04.eecs.tufts.edu>
2024-11-25 15:24:35 -0500

    pattern gen changes

--- source/impl_1/pattern_gen.vhd
+++ source/impl_1/pattern_gen.vhd
@@ -23,17 +23,29 @@ component fruitROM is
 	  color : out std_logic_vector(5 downto 0)
   );
 end component;
-signal fruit_1_tl_row : unsigned (4 downto 0) := "00000"; -- TODO update: 50 wide , 2 to 47 
-signal fruit_1_tl_col : unsigned (4 downto 0) := "00000"; -- TODO update: 50 wide , 2 to 47 
+signal fruit_1_tl_row : unsigned (9 downto 0) := "00000"; -- TODO update: 50 wide , 2 to 47 
+signal fruit_1_tl_col : unsigned (9 downto 0) := "00000"; -- TODO update: 50 wide , 2 to 47 
 signal fruit_1_RGB : unsigned (5 downto 0); -- we will get these values from all the different ROM and compare to decide what to render
 -- signal fruit : unsigned;
 -- type COLOR is (BLUEBERRY, CHERRY, ORANGE, GRAPEFRUIT , WATERMELLON);
 -- signal color : COLOR := BLUEBERRY;
+signal fruit_row : unsigned (4 downto 0) := "00000";
+signal fruit_col : unsigned (4 downto 0) := "00000";
+signal get_row : unsigned (4 downto 0) := "00000";
+signal get_col : unsigned (4 downto 0) := "00000";
+signal valid_pos : std_logic;
 
 begin
 	--center <= "00111";
 
-	fruit_1 : fruitROM port map(row - fruit_1_tl_row , col - fruit_1_tl_col, "100000", clk, fruit_1_RGB);
+	fruit_row <= unsigned(row) - fruit_1_tl_row;
+	fruit_col <= unsigned(col) - fruit_1_tl_col;
+	valid_pos <= (fruit_row(9 downto 5) = 5d"0") and (fruit_col(9 downto 5) = 5d"0");
+	
+	get_row <= fruit_row when valid_pos else "11111";
+	get_col <= fruit_col when valid_pos else "11111";
+	
+	fruit_1 : fruitROM port map(get_row , get_col, "100000", clk, fruit_1_RGB);
 	
 	
 	RGB <= "000000" when valid = '0' -- can be changed here and below
